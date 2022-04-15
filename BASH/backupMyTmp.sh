795aa3aa373859207a48dcec820661ae9b385ea2
Gunslinger489 <lucasg3.gdh@gmail.com>
2022-04-15 17:34:20 -0300

    Added more features

--- BASH/backupMyTmp.sh
+++ BASH/backupMyTmp.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# This bash script is used to backup the directory tmp inside the home user
+# This bash script is used to backup the directory tmp inside the user's home to /tmp/
 
-user=$(whoami)
+if [ -z $1 ]; then
+    user=$(whoami)
+else
+    if [ ! -d "/home/$1/tmp" ]; then
+        echo "Requested $1 user home directory doesn't exist."
+        exit 1
+    fi
+    user=$1
+fi
+
 input=/home/${user}/tmp
 output=/tmp/${user}_home_$(date +%d-%m-%Y_%H%M%S).tar.gz
 
@@ -16,14 +25,32 @@ function total_directories {
     find $1 -type d | wc -l
 }
 
+function total_archived_directories {
+    tar -tzf $1 | grep /$ | wc -l
+}
+
+function total_archived_files {
+    tar -tzf $1 | grep -v /$ | wc -l
+}
+
 tar -czf $output $input 2> /dev/null
 
-echo -n "Files to be include:"
-total_files $input
-echo -n "Directories to be included:"
-total_directories $input
+src_files=$( total_files $input )
+src_directories=$( total_directories $input )
 
-echo "Backup of $input completed!"
+arch_files=$( total_archived_files $output )
+arch_directories=$( total_archived_directories $output )
 
-echo "Details about the output backup file:"
-ls -l $output
+
+echo "Files to be included: $src_files"
+echo "Directories to be included: $src_directories"
+echo "Files archived: $arch_files"
+echo "Directories archived: $arch_directories"
+
+if [ $src_files -eq $arch_files ]; then
+    echo "Backup of $input completed!"
+    echo "Details about the output backup file:"
+    ls -l $output
+else
+    echo "Backup of $input failed!"
+fi
