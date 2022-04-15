ecd34ea17365e4583c62cedb6222aa1a5727e4fa
Gunslinger489 <lucasg3.gdh@gmail.com>
2022-04-15 17:38:27 -0300

    robot Barista

---
+++ Python/robotBarista.py
@@ -0,0 +1,15 @@
+#Let's start a coffee together!! We´re going to build a coffee shop using some new Python programming concepts!!
+
+#Let's build robot Barista!!
+
+print("Hello, welcome to NetworkChuck coffee!!!!")
+
+name = input("What's your name?\n")
+
+print(f"Hello {name}, thank you so much for coming in today.")
+
+menu = "Black Coffee\nEspresso\nLatte\nCappucino"
+
+print (name + ", What would you like from our menu today? Here is what we are serving.\n" + menu)
+
+
