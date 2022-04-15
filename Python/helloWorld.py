15845173a835abe685bbd8354e9e8d460aa1f1f4
Gunslinger489 <lucasg3.gdh@gmail.com>
2022-04-15 17:37:25 -0300

    Hello World

---
+++ Python/helloWorld.py
@@ -0,0 +1,7 @@
+# Hey... There is nothing here. WAKE UP!
+print("Hello World!")
+print ('I am Iron Man')
+print ("No, I am Tony Start")
+
+# We are practicing Python ... IT'S AWESOME!!!
+print("I am Iron Man.\nNo, I am Tony Stark\nNo, I am Poppy")
